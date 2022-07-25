0f9c77fba5416c50a0c320da2f1bde7965ea4402
caioclavico <caiohclavico@gmail.com>
2022-07-25 18:36:14 -0400

    feat: created cart views

--- movies/urls.py
+++ movies/urls.py
@@ -2,4 +2,11 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path('movies/', views.MovieView.as_view())]
+urlpatterns = [
+    path("movies/", views.MovieView.as_view()),
+    path("cart/", views.CartListView.as_view()),
+    path("cart/pay/", views.PaymentView.as_view()),
+    path("cart/pending/", views.CartListPendingView.as_view()),
+    path("cart/add/<str:movie_uuid>/", views.CartAddView.as_view()),
+    path("cart/<str:cart_uuid>/", views.CartUuidView.as_view()),
+]
